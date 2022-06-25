bdb60e11f75253c08645de0580454107729eacf5
jaffer601 <jaffersadiq601@mail.com>
2022-06-25 20:43:21 +0530

    javascript variables

---
+++ firstscript.js
@@ -0,0 +1,42 @@
+//variables & primitive data types.
+/*types of varibles in javascript
+1. var 
+2. let
+3. const */
+
+//string-must be in quotes
+
+var firstname = "Hi this is sadiq";
+console.log(typeof firstname, firstname);
+
+//string concatenation
+
+var a = "access";
+var b = "im learing full stack web developer";
+console.log(a + b); 
+
+//number - interge or floating value
+
+var first_number = 786 ;
+console.log(typeof first_number, first_number);
+
+// boolean-- true or false
+
+var first99=true;
+console.log(typeof first99,first99);
+
+//undefined 
+var condition=undefined;
+console.log(typeof condition ,condition); 
+
+// null -- here type of null ""object"
+
+var condition=null;
+HTMLFormControlsCollection.log(typeof condition,condition);
+//here output will be object
+ 
+
+
+
+
+
